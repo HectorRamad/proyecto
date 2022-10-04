7ab628bd3515586613c861c01ef67a8edba5c117
AntonioEspindolaAguirre <114432837+AntonioEspindolaAguirre@users.noreply.github.com>
2022-10-03 23:13:20 -0500

    Add files via upload

---
+++ materialize/blog.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog</title>
+     <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" href="css/materialize.min.css">
+
+     <!-- Compiled and minified JavaScript -->
+     <script src="js/materialize.min.js"></script>
+             
+</head>
+<body>
+
+    <nav class=" black accent-3">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo center " >Blog</a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="sass.html"></a></li>
+          </ul>
+        </div>
+      </nav>
+      
+      <!----------------------------------------------------------------->
+       <div class="center"> 
+       <br><br><br>
+        <img src="img/logo.png"  alt="">
+       </div>
+       <!----------------------------------------------------------------->
+
+
+       <div>
+        <h2 class="center text-blue">Razones por las que tener una página web profesional</h2>
+      </div>
+<br><br><br>
+ 
+      <!----------------------------------------------------------------->
+     <div class="center">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/img1.png" width="300" height="300" >
+          <span class="card-title white-text">Genera credibilidad de negocio</span>
+        </div>
+        <div class="card-content">
+          <p>
+Cada vez más personas buscan en internet lo que quieren, soluciones a sus problemas, comparar precios o simplemente obtener información sobre una tienda, restaurante o empresa antes de comprar. Si no disponen de un sitio web, los clientes potenciales acudirán a sus competidores que sí disponen de una página web.
+
+Tener un sitio web no solo proporcionará la información que está buscando el usuario, sino que le da credibilidad al negocio. Transmitir seguridad, estabilidad y confianza en un lugar abierto las 24 horas los 365 días del año es algo difícil de conseguir en un negocio físico, además de suponer unos costes muy elevados de querer realizar ese servicio.
+
+</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<br><br><br>
+     
+      <!----------------------------------------------------------------->
+      
+      <div class="center">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/img2.png" >
+          <span class="card-title black-text">Ahorrar dinero a largo plazo</span>
+         
+        </div>
+        <div class="card-content">
+          <p>
+Generalmente los propietarios de las pequeñas empresas, especialmente las que acaban de comenzar, piensan que no pueden asumir el coste que conlleva el desarrollo de una página web profesional. Realmente no hacerlo sería un error. El costo de diseñar un sitio web varía dependiendo de las necesidades y funcionalidades que se le quieran dar, si se realiza correctamente y se emplea un gestor cómo wordpress que permita al cliente agregar y cambiar su propio contenido regularmente el coste del desarrollo habrá valido la pena, ya que con este sistema el cliente es capaz de ahorrarse dinero en actualizaciones.
+
+</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ <br><br><br>
+      <!----------------------------------------------------------------->
+      <div class="center">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/img3.png" >
+          <span class="card-title black-text">Mejorar el servicio al cliente</span>
+         
+        </div>
+        <div class="card-content">
+          <p>
+
+Un sitio web es una excelente manera de proporcionar un servicio de valor añadido a los clientes, y es una buena manera de hacer que vuelvan cuando necesiten de nuevo los productos o servicios ofrecidos.
+
+No importa qué tipo de producto se venda en el sitio web, en esta se puede incluir una sección (o directamente en el blog) en la que compartir consejos con los clientes sobre cómo usar o cuidar los productos que compraron.
+
+
+</p>
+        </div>
+      </div>
+    </div>
+  </div>
+              
+      <!----------------------------------------------------------------->
+
+<nav class=" black accent-3">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo center " >Blog</a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="sass.html"></a></li>
+          </ul>
+        </div>
+      </nav>
+</body>
+</html>
