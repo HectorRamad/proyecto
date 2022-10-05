ee31fb8cf240fa3b168f2b0594ba89eda853b13a
AntonioEspindolaAguirre <114432837+AntonioEspindolaAguirre@users.noreply.github.com>
2022-10-05 00:57:21 -0500

    Add files via upload

---
+++ materialize/blog.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+   
+   <title>Document</title>
+</head>
+<body>
+	    <!-- nabvar -->
+
+	<div class="nabvar-fixed">
+	  <nav class="black">
+		<div class="nav-wrper container">
+			<a href="#" class="brand-logo">JODHAR</a>
+			<a href="#" data-taget="menu-responsive" class="sidenav-trigger"></a>
+
+			<ul class="right  hide-on-med-and-down">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">xxx</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+
+			
+		</div>	        
+	  </nav>
+	</div>
+
+
+    <!--div g -->
+
+	<div class="container section">
+
+<!-- card n1 -->
+
+		<div class="col s12 m7">
+    <h2 class="header">Desarollo web ¿Que es?</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/img3.png" class="responsive-img">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>Desarrollo web significa construir y mantener sitios web; es el trabajo que tiene lugar en un segundo plano y que permite que una web tenga una apariencia impecable, un funcionamiento rápido y un buen desempeño para permitir la mejor experiencia de usuario. Los desarrolladores web son como duendes con poderes: nunca los ves, pero son los que hacen que todo esté bien y funcione de manera rápida y eficiente.
+          	<br>
+ 
+          Los conocimientos y habilidades vinculados al desarrollo web son los más demandados y también los mejor pagados. Se trata de una carrera con muchas posibilidades y salidas. 
+
+</p>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+
+<!-- card n2 -->
+
+
+		<div class="col s12 m7">
+    <h2 class="header">La presencia Online brinda una relación activa entre la empresa y los clientes</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/img1.png" class="responsive-img">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>
+            Si una empresa está pendiente de su sitio web y sus redes sociales, sin dudas ofrecerá una mejor atención a sus clientes porque podrá responderles consultas de forma rápida y dirigida. Además podrá permitirles a sus usuarios que dejen comentarios o sugerencias para que la compañía pueda tomar en cuenta y modificar.
+
+            Aquellos usuarios o clientes que estén satisfechos y conformes con los productos que hemos ofrecido, podrán dejar sus comentarios o valoraciones e inclusive recomendar ese producto a un amigo a través de las redes sociales. Este efecto viral será el que nos permitirá llegar a nuevos mercados que antes eran impensados.
+
+            La web será el instrumento para entregar el contenido apropiado al usuario, en el momento apropiado. Esto es dar un paso atrás al marketing tradicional que perseguía al usuario, 
+
+          </p>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+		
+<!-- cardn3 -->
+
+  <div class="col s12 m7">
+    <h2 class="header">Beneficios de una página web</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/img2.png" class="responsive-img">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>Cuando queremos buscar algo, acudimos directamente a internet, y es que esto se ha convertido en una acción indispensable. Por eso, si tienes un negocio y aún no estás en el mundo digital, queremos contarte los 9 beneficios de una página web. <br><br>
+          	
+          	El tener un sitio web es como tener tu negocio abierto 24/7, lo que hace que tus clientes puedan revisar tu catálogo de productos o servicios en cualquier momento. De esta forma puedes generar leads y realizar ventas fuera del horario convencional <br><br>
+ 
+    
+          	
+        <ol>
+        	<li>Beneficios de una página web</li><br>
+        	<li>Mejorar la imagen de la marca</li><br>
+        	<li>Posicionar la marca</li><br>
+        	<li>Fideliza a tus clientes</li><br>
+        	<li>Escaparate virtual</li><br>
+        	<li>Posiciona en los buscadores</li><br>
+        	<li>Servicio 24 horas</li><br>
+        	<li>Incrementa las ventas</li><br>
+        </ol>
+
+          </p>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+ 
+
+		
+	</div>
+
+
+
+
+
+	 <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script >
+    	document.addEventListener('DOMContentLoaded', function() {
+
+    		M.AutoInit();
+
+        });
+    </script>
+
+
+</body>
+</html>
